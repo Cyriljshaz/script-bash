f7442df60e637270bb05ddc14f3ea465abb885bb
Cyril JULIEN <49750071+Cyriljshaz@users.noreply.github.com>
2024-10-02 20:44:24 +0200

    inital

---
+++ JPGtoPlaceHolder.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Répertoire où se trouvent les images
+INPUT_DIR="."  # Remplacer par le chemin du répertoire d'images si nécessaire
+
+# Pour chaque fichier JPG dans le répertoire
+for file in "$INPUT_DIR"/*.jpg; do
+  # Récupère le nom de fichier sans l'extension
+  filename="${file%.jpg}"
+
+  echo "Traitement de l'image : $file"
+
+  # Placeholder petit (320px)
+  magick convert "$file" -resize 320 -quality 100 "$filename-placeholder-small.jpg"  # Petit en JPG
+
+  # Placeholder moyen (640px)
+  magick convert "$file" -resize 640 -quality 100 "$filename-placeholder-medium.jpg"  # Moyen en JPG
+
+  # Placeholder large (1024px)
+  magick convert "$file" -resize 1024 -quality 100 "$filename-placeholder-large.jpg"  # Large en JPG
+
+  # Placeholder très grand (1920px)
+  magick convert "$file" -resize 1920 -quality 100 "$filename-placeholder-xlarge.jpg"  # Très grand en JPG
+
+  echo "4 versions de l'image générées pour $file (4 tailles en JPG)"
+done
+
+echo "Optimisation terminée."
---
+++ PNGtoPlaceHolder.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Répertoire où se trouvent les images
+INPUT_DIR="."  # Remplacer par le chemin du répertoire d'images si nécessaire
+
+# Pour chaque fichier JPG dans le répertoire
+for file in "$INPUT_DIR"/*.jpg; do
+  # Récupère le nom de fichier sans l'extension
+  filename="${file%.jpg}"
+
+  echo "Traitement de l'image : $file"
+
+  # Placeholder petit (320px)
+  magick convert "$file" -resize 320 -quality 100 "$filename-placeholder-small.png"  # Petit en PNG
+
+  # Placeholder moyen (640px)
+  magick convert "$file" -resize 640 -quality 100 "$filename-placeholder-medium.png"  # Moyen en PNG
+
+  # Placeholder large (1024px)
+  magick convert "$file" -resize 1024 -quality 100 "$filename-placeholder-large.png"  # Large en PNG
+
+  # Placeholder très grand (1920px)
+  magick convert "$file" -resize 1920 -quality 100 "$filename-placeholder-xlarge.png"  # Très grand en PNG
+
+  echo "4 versions de l'image générées pour $file (4 tailles en PNG)"
+done
+
+echo "Optimisation terminée."
