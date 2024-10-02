9adf515d5866dea8bdd687e8013c14cca67796d0
Cyril JULIEN <49750071+Cyriljshaz@users.noreply.github.com>
2024-10-02 20:57:52 +0200

    magick convert was depreciate

--- JPGToWebP.sh
+++ JPGToWebP.sh
@@ -11,16 +11,16 @@ for file in "$INPUT_DIR"/*.jpg; do
   echo "Traitement de l'image : $file"
 
   # Générer la version mobile (300px)
-  magick convert "$file" -resize 300 -quality 85 "$filename-mobile.jpg"  # Version mobile en JPG
-  magick convert "$file" -resize 300 -quality 85 "$filename-mobile.webp"  # Version mobile en WebP
+  magick "$file" -resize 300 -quality 85 "$filename-mobile.jpg"  # Version mobile en JPG
+  magick "$file" -resize 300 -quality 85 "$filename-mobile.webp"  # Version mobile en WebP
 
   # Générer la version tablette (768px)
-  magick convert "$file" -resize 768 -quality 85 "$filename-tablet.jpg"  # Version tablette en JPG
-  magick convert "$file" -resize 768 -quality 85 "$filename-tablet.webp"  # Version tablette en WebP
+  magick "$file" -resize 768 -quality 85 "$filename-tablet.jpg"  # Version tablette en JPG
+  magick "$file" -resize 768 -quality 85 "$filename-tablet.webp"  # Version tablette en WebP
 
   # Générer la version ordinateur (1200px)
-  magick convert "$file" -resize 1200 -quality 85 "$filename-desktop.jpg"  # Version ordinateur en JPG
-  magick convert "$file" -resize 1200 -quality 85 "$filename-desktop.webp"  # Version ordinateur en WebP
+  magick "$file" -resize 1200 -quality 85 "$filename-desktop.jpg"  # Version ordinateur en JPG
+  magick "$file" -resize 1200 -quality 85 "$filename-desktop.webp"  # Version ordinateur en WebP
 
   echo "6 versions de l'image générées pour $file (mobile, tablette, ordinateur en JPG et WebP)"
 done
--- PNGToWebP.sh
+++ PNGToWebP.sh
@@ -11,16 +11,16 @@ for file in "$INPUT_DIR"/*.png; do
   echo "Traitement de l'image : $file"
 
   # Générer la version mobile (300px)
-  magick convert "$file" -resize 300 -quality 85 "$filename-mobile.png"  # Version mobile en PNG
-  magick convert "$file" -resize 300 -quality 85 "$filename-mobile.webp"  # Version mobile en WebP
+  magick "$file" -resize 300 -quality 85 "$filename-mobile.png"  # Version mobile en PNG
+  magick "$file" -resize 300 -quality 85 "$filename-mobile.webp"  # Version mobile en WebP
 
   # Générer la version tablette (768px)
-  magick convert "$file" -resize 768 -quality 85 "$filename-tablet.png"  # Version tablette en PNG
-  magick convert "$file" -resize 768 -quality 85 "$filename-tablet.webp"  # Version tablette en WebP
+  magick "$file" -resize 768 -quality 85 "$filename-tablet.png"  # Version tablette en PNG
+  magick "$file" -resize 768 -quality 85 "$filename-tablet.webp"  # Version tablette en WebP
 
   # Générer la version ordinateur (1200px)
-  magick convert "$file" -resize 1200 -quality 85 "$filename-desktop.png"  # Version ordinateur en PNG
-  magick convert "$file" -resize 1200 -quality 85 "$filename-desktop.webp"  # Version ordinateur en WebP
+  magick "$file" -resize 1200 -quality 85 "$filename-desktop.png"  # Version ordinateur en PNG
+  magick "$file" -resize 1200 -quality 85 "$filename-desktop.webp"  # Version ordinateur en WebP
 
   echo "6 versions de l'image générées pour $file (mobile, tablette, ordinateur en PNG et WebP)"
 done
